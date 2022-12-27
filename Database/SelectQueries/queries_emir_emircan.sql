e7d13c75cc70653dbce62def120fbb19b32210a4
Emir Çetin MEMİŞ <zeymenak@gmail.com>
2022-12-27 15:24:38 +0300

    add arda's queries

--- Database/SelectQueries/queries_emir_emircan.sql
+++ Database/SelectQueries/queries_emir_emircan.sql
@@ -8,3 +8,16 @@
 --  	Some real-life examples 	- 5 examples written.
 --  	Some intermediate examples 	- 9 examples written.
 --  	TOTAL						- 30 examples written.
+
+-- To list the posts owned by a user.
+SELECT * FROM Post WHERE UserId = '<user_id>';
+-- To list the comments of a post.
+SELECT * FROM Comment WHERE PostId = '<post_id>';
+-- To list the users followed by a user.
+SELECT FollowedID FROM Follow WHERE FollowerID = '<user_id>';
+-- To list the top 10 posts with the most interaction.
+SELECT * FROM Post ORDER BY Likes + Comments + Shares DESC LIMIT 10;
+-- To list the comments made by a user.
+SELECT * FROM Comment WHERE UserId = '<user_id>';
+-- To list the users blocked by a user.
+SELECT BlockedID FROM Block WHERE BlockerID = '<user_id>';
