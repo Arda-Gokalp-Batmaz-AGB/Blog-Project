4ace20d38edf429bd0bff5241cb9af4c864ec86f
Arda Gokalp Batmaz <ardagokalpbatmaz@hotmail.com>
2022-12-29 10:16:20 +0300

    add repo pattern on post

--- Web/DictionaryService/Data/Repositories/PostRepository.cs
+++ Web/DictionaryService/Data/Repositories/PostRepository.cs
@@ -1,6 +1,70 @@
-﻿namespace DictionaryService.Data.Repositories
+﻿using DictionaryService.Data.Entities;
+using DictionaryService.Models.BindingModel;
+using DictionaryService.Models.DTO;
+using System.Data.Common;
+using Npgsql;
+using Microsoft.Extensions.Configuration;
+
+namespace DictionaryService.Data.Repositories
 {
-    public class PostRepository
+    public class PostRepository : IPostRepository
     {
+        private readonly IConfiguration _configuration;
+        private readonly string CS;
+        public PostRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            CS = _configuration.GetConnectionString("PostGreConnection");
+        }
+        //private readonly string CS = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        public void DeletePost(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PostDTO> GetAllPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetPostById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Post> GetPostComments(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Post> InsertPost(AddUpdatePostBindingModel model)
+        {
+            //var cs = "Host=localhost;Username=postgres;Password=tabak34b;Database=Blog"
+
+            using var con = new NpgsqlConnection(CS);
+            con.Open();
+
+            using var cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+
+            //cmd.CommandText = @"INSERT INTO public.""Post""(""Title"", ""AuthorID"") VALUES ('Audi7','8080ff0e-e3dd-46a6-adb5-cdd331458e0c');";
+            try
+            {
+                cmd.CommandText = $"INSERT INTO public.\"Post\"(\"Title\", \"AuthorID\") VALUES (@Title,@AuthorID);";
+                cmd.Parameters.AddWithValue("Title", model.Title);
+                cmd.Parameters.AddWithValue("AuthorID", model.AuthorID);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message.ToString());
+            }
+            return new Post() { Title = model.Title,AuthorID=model.AuthorID};
+        }
+
+        public void UpdatePost(Post updateApp)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Web/DictionaryService/Services/PostService.cs
+++ Web/DictionaryService/Services/PostService.cs
@@ -1,6 +1,47 @@
-﻿namespace DictionaryService.Data.Repositories
+﻿using DictionaryService.Data.Entities;
+using DictionaryService.Models.BindingModel;
+using DictionaryService.Models.DTO;
+
+namespace DictionaryService.Data.Repositories
 {
-    public class IPostRepository
+    public class PostService : IPostService
     {
+        private IPostRepository _repository;
+        public PostService(IPostRepository repository)
+        {
+            _repository = repository;
+        }
+        public Task<PostDTO> GetPost(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Post> InsertPost(AddUpdatePostBindingModel model)
+        {
+            try 
+            {
+                Post post = await _repository.InsertPost(model);
+                return post;
+            }
+            catch  (Exception ex) 
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<PostDTO> ListAllPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PostDTO> ListFilteredPosts(string[] categories)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CommentDTO> ListPostComments()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
