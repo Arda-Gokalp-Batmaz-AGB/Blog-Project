c5e6ecfa8a25b4bf268b0b44a63e23bad28be1f4
Arda Gokalp Batmaz <ardagokalpbatmaz@hotmail.com>
2022-12-30 23:44:17 +0300

    add interaction for post

--- Web/DictionaryService/ClientApp/src/app/post/post.component.ts
+++ Web/DictionaryService/ClientApp/src/app/post/post.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { responsePost } from '../models/responsePost';
+import { BlogService } from '../services/blog.service';
+import { UserService } from '../services/user.service';
+import { responseUser } from '../models/responseUser';
+import { responseComment } from '../models/responseComment';
 
 @Component({
   selector: 'app-post',
@@ -8,10 +12,36 @@ import { responsePost } from '../models/responsePost';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService : UserService,private blogService : BlogService) { }
 
+  currentUser? : responseUser | null
   @Input() post! : responsePost
   ngOnInit(): void {
   }
 
+  updatePost(type : string) : void
+  {
+    this.currentUser = this.userService.currentUserValue
+    if(!this.currentUser!.id)
+    {
+      this.userService.returnLoginPage();
+      return;
+    }
+    this.blogService.interactComment(this.post.comments[0].id,type,this.currentUser!.id).subscribe({
+      next: (x : responseComment) => {
+
+      this.post.comments[0].likeCount = x.likeCount;
+      this.post.comments[0].dislikeCount =  x.dislikeCount;
+    },
+    error: (err) => {
+      console.log('Error in interaction');
+      console.log(err);
+    },
+  });
+  }
+
+  interactionPost(interecactionType : string) : void
+  {
+    this.updatePost(interecactionType);
+  }
 }
